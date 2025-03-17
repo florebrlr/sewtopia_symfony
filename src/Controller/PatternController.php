650c5e4b9f30c06ed3d45f1396f0202da19bc74a
Pink-Tea-Rex <flore.bruneliere.pro@gmail.com>
2025-03-17 19:23:11 +0100

    :white_check_mark: création -PatternController - detail.html.twig - list.html.twig

---
+++ src/Controller/PatternController.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/patterns/', name: 'pattern_')]
+class PatternController extends AbstractController
+{
+    //route de la liste des patrons
+    #[Route('/', name: 'list', methods: ['GET'])]
+    public function list(): Response
+    {
+        return $this->render('pattern/list.html.twig');
+    }
+
+    //route d'un patron
+    #[Route('/{id}', name: 'detail', methods: ['GET'])]
+    public function detail(): Response
+    {
+        return $this->render('pattern/detail.html.twig');
+    }
+
+}
