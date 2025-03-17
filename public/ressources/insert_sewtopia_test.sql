853fc233e292e3ec2b0777fda2462cf8cc97b787
Pink-Tea-Rex <flore.bruneliere.pro@gmail.com>
2025-03-17 21:02:19 +0100

    :wrench: insert 10 datas (fake)

---
+++ public/ressources/insert_sewtopia_test.sql
@@ -0,0 +1,11 @@
+INSERT INTO `pattern` (`title`, `commentary`, `author`, `is_printed`, `date_produced`) VALUES
+('Pattern A', 'This is a commentary for pattern A', 'Author 1', 1, '2025-03-01 10:00:00'),
+('Pattern B', 'This is a commentary for pattern B', 'Author 2', 0, '2025-03-02 11:15:00'),
+('Pattern C', 'This is a commentary for pattern C', 'Author 3', 1, '2025-03-03 14:30:00'),
+('Pattern D', 'This is a commentary for pattern D', 'Author 4', 0, '2025-03-04 16:45:00'),
+('Pattern E', 'This is a commentary for pattern E', 'Author 5', 1, '2025-03-05 09:00:00'),
+('Pattern F', 'This is a commentary for pattern F', 'Author 6', 0, '2025-03-06 12:10:00'),
+('Pattern G', 'This is a commentary for pattern G', 'Author 7', 1, '2025-03-07 13:25:00'),
+('Pattern H', 'This is a commentary for pattern H', 'Author 8', 0, '2025-03-08 15:40:00'),
+('Pattern I', 'This is a commentary for pattern I', 'Author 9', 1, '2025-03-09 17:55:00'),
+('Pattern J', 'This is a commentary for pattern J', 'Author 10', 0, '2025-03-10 08:20:00');
